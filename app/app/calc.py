af6c2d7f3c2eb4cc0e571f0c1a90e37173290d88
Sivar-Developer <fragmatica.dc@gmail.com>
2022-10-26 10:57:17 +0300

    Fix linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,12 +3,11 @@ Calculator functions
 """
 
 
-def add(x, y):  
+def add(x, y):
     """Add x and y return result."""
     return x + y
 
 
-def subtract(x, y):  
+def subtract(x, y):
     """ Subtract x from y and return result. """
     return y - x
-
