46b1a370fe24a9996172f3914eda5d81ef353347
Sivar-Developer <fragmatica.dc@gmail.com>
2022-10-27 16:24:12 +0300

    Fix linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,10 +3,9 @@ Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2
+from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
-from django.db.utils import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
