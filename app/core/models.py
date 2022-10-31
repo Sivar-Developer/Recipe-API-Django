9a6f96899537dcf4b2e9871d1417bcb18032be76
Sivar-Developer <fragmatica.dc@gmail.com>
2022-10-31 11:13:01 +0300

    Fix lint

--- app/core/models.py
+++ app/core/models.py
@@ -12,13 +12,13 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for user"""
 
-    def create_user(self, email, password = None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
             raise ValueError('User must have an email address')
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -27,7 +27,7 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
