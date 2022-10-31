891f9068d986c53ddf003ec4598670e631a9ecdc
Sivar-Developer <fragmatica.dc@gmail.com>
2022-10-31 16:40:04 +0300

    Fix lint

--- app/core/admin.py
+++ app/core/admin.py
@@ -6,6 +6,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users"""
     ordering = ['id']
@@ -39,5 +40,6 @@ class UserAdmin(BaseUserAdmin):
             )
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
