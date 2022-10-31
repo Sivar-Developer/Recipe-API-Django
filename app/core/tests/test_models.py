03ca690b64b8be53662249e23e064672c7970963
Sivar-Developer <fragmatica.dc@gmail.com>
2022-10-31 11:19:07 +0300

    Fix lint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,20 +1,20 @@
 """
 Test for models.
 """
-import email
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
     """Test Models"""
 
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email = 'test@example.com',
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
@@ -31,7 +31,6 @@ class ModelTests(TestCase):
     #     for email, expected in sample_emails:
     #         user = get_user_model().objects.create_user(email, 'sample123')
     #         self.assertEqual(user.email, expected)
-
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
