178acbba000121b81d14edfd714ed91a9fabd8ce
Sivar-Developer <fragmatica.dc@gmail.com>
2022-10-31 11:22:09 +0300

    Fix lint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,9 +4,9 @@ Test for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test Models"""
-
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
