4005974370c989156643c34350a05b3d047dadd2
Sivar-Developer <fragmatica.dc@gmail.com>
2022-10-31 11:15:07 +0300

    Fix lint

--- app/core/models.py
+++ app/core/models.py
@@ -34,10 +34,10 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
-    email = models.EmailField(max_length = 255, unique = True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
