bdf211d39b509f757916b7d2e6005c0a2e8d27d8
Sivar-Developer <fragmatica.dc@gmail.com>
2022-11-01 11:29:55 +0300

    Code check

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,6 @@
 """
 Django command to wait for the database to be available.
 """
-
 import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
@@ -11,10 +10,10 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """ Django command to wait for database."""
+    """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        """Entrypoint for commands."""
+        """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
         while db_up is False:
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,7 +10,7 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
-        email = 'test@example.com',
+        email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
             email=email,
