00824faed50861d27f3a8df02d596d266f05c6bf
Sivar-Developer <fragmatica.dc@gmail.com>
2022-11-01 11:05:05 +0300

    Code check

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,10 +4,10 @@ Calculator functions
 
 
 def add(x, y):
-    """Add x and y return result."""
+    """Add x and y and return result."""
     return x + y
 
 
 def subtract(x, y):
-    """ Subtract x from y and return result. """
+    """Subtract x from y and return result."""
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -4,13 +4,13 @@ from app import calc
 
 
 class CalcTests(SimpleTestCase):
-    """ Test the calc module. """
+    """Test the calc module."""
     def test_add_number(self):
-        """ Test adding numbers together """
+        """Test adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        """ Test subtracting numbers. """
+        """Test subtracting numbers."""
         res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
