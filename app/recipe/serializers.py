54ed5452382a13622c1ab8907ddfa79bcb927fb4
Sivar-Developer <fragmatica.dc@gmail.com>
2022-11-08 14:07:36 +0300

    Tag serializer before recipe serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -9,6 +9,16 @@ from core.models import (
 )
 
 
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags."""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     """Serializer for recipes."""
 
@@ -23,12 +33,3 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
-
-
-class TagSerializer(serializers.ModelSerializer):
-    """Serializer for tags."""
-
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
-        read_only_fields = ['id']
